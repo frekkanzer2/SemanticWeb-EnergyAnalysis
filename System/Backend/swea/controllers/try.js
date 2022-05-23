a125726ea4b7e29aecf3cb9dde204521a6d7388d
Carmine Ferrara <49783581+CFerrara98@users.noreply.github.com>
2022-05-23 23:23:09 +0200

    General Backend Update
    Adding method and plugin to create and query a local end-point from protegè ontology

--- System/Backend/swea/controllers/try.js
+++ System/Backend/swea/controllers/try.js
@@ -1,7 +1,7 @@
-// Parse a SPARQL query to a JSON object
 const QueryEngine = require('@comunica/query-sparql').QueryEngine;
 const myEngine = new QueryEngine();
 
+//exernal end point
 exports.test = async (req, res, next) => {
     
     const result = [];
@@ -17,6 +17,127 @@ exports.test = async (req, res, next) => {
         
         // Obtaining values
         result.push(binding.get('o').value);
+        
+    });
+
+    bindingsStream.on('end', () => {
+        // The data-listener will not be called anymore once we get here.
+        console.log("Ended");
+
+        return res.json({
+            queryResult : result
+        });
+
+    });
+
+    bindingsStream.on('error', (error) => {
+        console.error(error);
+    });
+
+}
+
+/*to set a local end point from a ttl file, follow this link: 
+    https://comunica.dev/docs/query/getting_started/setup_endpoint/
+
+    Command 1 - require package: npm install -g @comunica/query-sparql-file
+    Command 2 - open "End Point Server" - comunica-sparql-file-http path/to/my/file.ttl
+
+    IMPORTANT: the exported file from protege, must be a .ttl
+    IMPORTANT: Make sure that the relative or absolute path of the file does not contain spaces...*/
+
+exports.localOntology = async (req, res, next) => {
+    
+    const result = [];
+
+    const bindingsStream = await myEngine.queryBindings(`
+            PREFIX swea: <http://www.semanticweb.org/abate/ontologies/2022/4/swea#>
+            PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+            SELECT ?s WHERE {
+            ?s ?p ?o
+            } LIMIT 100`, {
+        sources: ['http://localhost:3000/sparql'],
+    });
+
+    bindingsStream.on('data', (binding) => {
+        
+        // Obtaining values
+        result.push(binding.get('s').value);
+        
+    });
+
+    bindingsStream.on('end', () => {
+        // The data-listener will not be called anymore once we get here.
+        console.log("Ended");
+
+        return res.json({
+            queryResult : result
+        });
+
+    });
+
+    bindingsStream.on('error', (error) => {
+        console.error(error);
+    });
+
+}
+
+exports.localOntology = async (req, res, next) => {
+    
+    const result = [];
+
+    const bindingsStream = await myEngine.queryBindings(`
+            PREFIX swea: <http://www.semanticweb.org/abate/ontologies/2022/4/swea#>
+            PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+            SELECT ?s WHERE {
+            ?s ?p ?o
+            } LIMIT 100`, {
+        sources: ['http://localhost:3000/sparql'],
+    });
+
+    bindingsStream.on('data', (binding) => {
+        
+        // Obtaining values
+        result.push(binding.get('s').value);
+        
+    });
+
+    bindingsStream.on('end', () => {
+        // The data-listener will not be called anymore once we get here.
+        console.log("Ended");
+
+        return res.json({
+            queryResult : result
+        });
+
+    });
+
+    bindingsStream.on('error', (error) => {
+        console.error(error);
+    });
+
+}
+
+exports.allCountry = async (req, res, next) => {
+    
+    const result = [];
+
+    const bindingsStream = await myEngine.queryBindings(`
+        PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+        PREFIX owl: <http://www.w3.org/2002/07/owl#>
+        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+        PREFIX swea: <http://www.semanticweb.org/abate/ontologies/2022/4/swea#>
+                
+        SELECT ?individual 
+        WHERE { 
+            ?individual rdf:type swea:Territory .
+        } LIMIT 100`, {
+        sources: ['http://localhost:3000/sparql'],
+    });
+
+    bindingsStream.on('data', (binding) => {
+        
+        // Obtaining values
+        result.push(binding.get('individual').value);
         
     });
 
--- System/Backend/swea/routes/try.js
+++ System/Backend/swea/routes/try.js
@@ -4,5 +4,7 @@ const router = express.Router();
 const tryController = require("../controllers/try");
 
 router.get("/trytest", tryController.test);
+router.get("/localTest", tryController.localOntology);
+router.get("/allCountry", tryController.allCountry);
 
 module.exports = router;
