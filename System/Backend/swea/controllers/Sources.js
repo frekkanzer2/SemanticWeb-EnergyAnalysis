0853a9fd3bea89cfad353ee27c6013eb6c32340a
Carmine Ferrara <49783581+CFerrara98@users.noreply.github.com>
2022-05-27 09:41:28 +0200

    Descrive endpoint 2

--- System/Backend/swea/controllers/Sources.js
+++ System/Backend/swea/controllers/Sources.js
@@ -1,4 +1,5 @@
 const QueryEngine = require('@comunica/query-sparql').QueryEngine;
+const SparqlClient = require('sparql-http-client')
 const myEngine = new QueryEngine();
 
 const EnergyDescription = "Renewable energy is energy that is collected from renewable resources that are naturally replenished on a human timescale. It includes sources such as sunlight, wind, rain, tides, waves, and geothermal heat. Renewable energy stands in contrast to fossil fuels, which are being used far more quickly than they are being replenished. Although most renewable energy sources are sustainable, some are not. For example, some biomass sources are considered unsustainable at current rates of exploitation. Renewable energy often provides energy in four important areas: electricity generation, air and water heating/cooling, transportation, and rural (off-grid) energy services. About 20% of humans' global energy consumption is renewables, including almost 30% of electricity. About 8% of energy consumption is traditional biomass, but this is declining. Over 4% of energy consumption is heat energy from modern renewables, such as solar water heating, and over 6% electricity. Globally there are over 10 million jobs associated with the renewable energy industries, with solar photovoltaics being the largest renewable employer. Renewable energy systems are rapidly becoming more efficient and cheaper and their share of total energy consumption is increasing, with a large majority of worldwide newly installed electricity capacity being renewable. In most countries, photovoltaic solar or onshore wind are the cheapest new-build electricity. Many nations around the world already have renewable energy contributing more than 20% of their energy supply. And many nations around the world already generate over half their electricity from renewables. National renewable energy markets are projected to continue to grow strongly in the coming decade and beyond.A few countries generate all their electricity using renewable energy.Renewable energy resources exist over wide geographical areas, in contrast to fossil fuels, which are concentrated in a limited number of countries. Deployment of renewable energy and energy efficiency technologies is resulting in significant energy security, climate change mitigation, and economic benefits. However renewables are being hindered by hundreds of billions of dollars of fossil fuel subsidies. In international public opinion surveys there is strong support for promoting renewable sources such as solar power and wind power. While many renewable energy projects are large-scale, renewable technologies are also suited to rural and remote areas and developing countries, where energy is often crucial in human development. As most of renewable energy technologies provide electricity, renewable energy is often deployed together with further electrification, which has several benefits: electricity can be converted to heat, can be converted into mechanical energy with high efficiency, and is clean at the point of consumption. In addition, electrification with renewable energy is more efficient and therefore leads to significant reductions in primary energy requirements. In 2021 China accounted for almost half of the increase in renewable electricity.";
@@ -77,7 +78,7 @@ exports.singleEnergyInf = async (req, res, next) => {
     FILTER ( LANG ( ?abstract) = 'en' )
     FILTER ( LANG ( ?name) = 'en' )
     }`, {
-        sources: ['http://dbpedia.org/sparql'],
+        sources: ['https://dbpedia.org/sparql'],
     });
 
     bindingsStream.on('data', (binding) => {
@@ -288,24 +289,21 @@ exports.singleSourceCriteriaRelated = async (req, res, next) => {
 
 //singleSourceCriteriaRelated
 // Energy Usage Critera that depends of input renewable source
-exports.describeSource = async (req, res, next) => {
+/*exports.describeSource = async (req, res, next) => {
     
     const sourceRes = req.query.res;
 
     var records = [];
 
-    const bindingsStream = await myEngine.queryBindings(`
+    const bindingsStream = await myEngine.queryQuads(`
         PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
         PREFIX owl: <http://www.w3.org/2002/07/owl#>
         PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
         PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
-        PREFIX swea: <http://www.semanticweb.org/abate/ontologies/2022/4/swea#>
-        PREFIX dbo: <http://dbpedia.org/ontology/#>
+        PREFIX dbo:  <http://dbpedia.org/ontology/>
         
-        DESCRIBE ?x
-        WHERE { <` + sourceRes + `> dbo:wikiPageWikiLink ?x}
-        Limit 1
-        `, {
+        DESCRIBE <` + sourceRes + `>
+        WHERE { <` + sourceRes + `> dbo:wikiPageWikiLink ?x} LIMIT 50`, {
         sources: ['http://dbpedia.org/sparql'],
     });
 
@@ -326,5 +324,50 @@ exports.describeSource = async (req, res, next) => {
     bindingsStream.on('error', (error) => {
         console.error(error);
     });
+
+}*/
+
+
+//singleSourceCriteriaRelated
+// Energy Usage Critera that depends of input renewable source
+exports.describeSource = async (req, res, next) => {
+    const sourceRes = req.query.res;
+    const results = [];
+
+    const endpointUrl = 'https://dbpedia.org/sparql'
+    const query = `
+        PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+        PREFIX owl: <http://www.w3.org/2002/07/owl#>
+        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+        PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+        PREFIX dbo:  <http://dbpedia.org/ontology/>
+        
+        DESCRIBE  <` + sourceRes + `>
+        WHERE { <` + sourceRes + `> dbo:wikiPageWikiLink ?x
+        FILTER ( LANG ( ?x) = 'en' ) } LIMIT 1
+    `
+    
+    const client = new SparqlClient({ endpointUrl })
+    const stream = await client.query.select(query)
+    
+    stream.on('data', row => {
+      var i = 0;
+      var key;
+      Object.entries(row).forEach(([key, value]) => {
+        i = i + 1;
+        var JSONkey = "res" + i; 
+        results.push(value.value);
+        console.log(`${key}: ${value.value} (${value.termType})`)
+      })
+    })
+
+    stream.on('end', row => {
+        return res.json({results: results});
+      })
+
+    
+    stream.on('error', err => {
+      console.error(err)
+    })
 
 }
